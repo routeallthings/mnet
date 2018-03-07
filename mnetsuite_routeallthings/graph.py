c677b4b8260ef3843cff9bf51fb058d7e9a92f46
routeallthings <30909318+routeallthings@users.noreply.github.com>
2018-03-07 15:39:37 -0600

    Added invalid character cleanup

--- mnetsuite_routeallthings/graph.py
+++ mnetsuite_routeallthings/graph.py
@@ -149,6 +149,9 @@ class mnet_graph:
 		
 		if node.name.endswith('.'):
 			node.name = node.name[:-1]
+			
+		# Invalid Character Cleanup
+		node.name = re.sub('[^A-Za-z0-9._-]+', '', node.name)
 
 		# verify this node isn't already in our visited
 		# list by checking for its hostname
